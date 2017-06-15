a687e58365709c7d69a2c52803c7f5c3dfa7c83c
Nicolas Juneau <nicolas.juneau@savoirfairelinux.com>
2017-06-15 15:56:29 -0400

    Added support for searching in documents and media

--- modules/flashlight-search-service/src/main/java/com/savoirfairelinux/flashlight/service/impl/search/result/DLFileEntrySearchResultProcessor.java
+++ modules/flashlight-search-service/src/main/java/com/savoirfairelinux/flashlight/service/impl/search/result/DLFileEntrySearchResultProcessor.java
@@ -1,22 +1,50 @@
 package com.savoirfairelinux.flashlight.service.impl.search.result;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.portlet.PortletRequest;
 import javax.portlet.PortletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 import com.liferay.document.library.kernel.model.DLFileEntry;
+import com.liferay.document.library.kernel.model.DLFileEntryType;
+import com.liferay.document.library.kernel.service.DLAppService;
+import com.liferay.document.library.kernel.service.DLFileEntryTypeLocalService;
+import com.liferay.document.library.kernel.util.DL;
+import com.liferay.document.library.kernel.util.DLUtil;
+import com.liferay.dynamic.data.mapping.model.DDMTemplate;
+import com.liferay.dynamic.data.mapping.service.DDMTemplateLocalService;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.repository.model.FileVersion;
 import com.liferay.portal.kernel.search.Document;
+import com.liferay.portal.kernel.search.Field;
 import com.liferay.portal.kernel.search.SearchContext;
 import com.liferay.portal.kernel.search.facet.Facet;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portlet.display.template.PortletDisplayTemplate;
 import com.savoirfairelinux.flashlight.service.configuration.FlashlightSearchConfiguration;
 import com.savoirfairelinux.flashlight.service.configuration.FlashlightSearchConfigurationTab;
+import com.savoirfairelinux.flashlight.service.impl.DocumentField;
+import com.savoirfairelinux.flashlight.service.impl.facet.DLFileEntryTypeFacet;
+import com.savoirfairelinux.flashlight.service.impl.search.result.template.DLFileEntryTemplateVariable;
 import com.savoirfairelinux.flashlight.service.model.SearchResult;
 import com.savoirfairelinux.flashlight.service.search.result.SearchResultProcessor;
 import com.savoirfairelinux.flashlight.service.search.result.exception.SearchResultProcessorException;
 
 /**
- * This processor is used to display file entries
+ * This processor is used to display file entries in the search results
  */
 @Component(
     service = SearchResultProcessor.class,
@@ -27,21 +55,130 @@ import com.savoirfairelinux.flashlight.service.search.result.exception.SearchRes
 )
 public class DLFileEntrySearchResultProcessor implements SearchResultProcessor {
 
+    @Reference
+    private DLFileEntryTypeLocalService dlFileEntryTypeService;
+
+    @Reference
+    private DLAppService dlAppService;
+
+    @Reference
+    private DDMTemplateLocalService ddmTemplateService;
+
+    @Reference
+    private Portal portal;
+
+    @Reference
+    private PortletDisplayTemplate portletDisplayTemplate;
+
     private static final String ASSET_TYPE = DLFileEntry.class.getName();
 
     @Override
     public Facet getFacet(SearchContext searchContext, FlashlightSearchConfiguration configuration, FlashlightSearchConfigurationTab tab) {
-        return null;
+        DLFileEntryTypeFacet facet = new DLFileEntryTypeFacet(searchContext);
+        Map<String, String> fileEntryTypes = tab.getDLFileEntryTypeTemplates();
+        long companyId = searchContext.getCompanyId();
+
+        ArrayList<Long> fileEntryTypeIds = new ArrayList<>(fileEntryTypes.size());
+        for(String fileEntryTypeUuid : fileEntryTypes.keySet()) {
+            List<DLFileEntryType> companyFileEntryTypes = this.dlFileEntryTypeService.getDLFileEntryTypesByUuidAndCompanyId(fileEntryTypeUuid, companyId);
+            if(companyFileEntryTypes.size() == 1) {
+                fileEntryTypeIds.add(companyFileEntryTypes.get(0).getFileEntryTypeId());
+            }
+        }
+
+        int idsLength = fileEntryTypeIds.size();
+        long[] ids = new long[idsLength];
+        for(int i = 0; i < idsLength; i++) {
+            ids[i] = fileEntryTypeIds.get(i);
+        }
+
+        facet.setValues(ids);
+        return facet;
     }
 
     @Override
     public SearchResult process(PortletRequest request, PortletResponse response, SearchContext searchContext, FlashlightSearchConfigurationTab configurationTab, Document searchResultDocument) throws SearchResultProcessorException {
-        return new SearchResult("Rendering!", "#", "Yay!");
+        long fileEntryTypeId = Long.parseLong(searchResultDocument.get(DocumentField.FILE_ENTRY_TYPE_ID.getName()));
+        SearchResult result;
+
+        try {
+            DLFileEntryType fileEntryType = this.dlFileEntryTypeService.getFileEntryType(fileEntryTypeId);
+            String fileEntryTypeUuid = fileEntryType.getUuid();
+            Map<String, String> configuredTemplates = configurationTab.getDLFileEntryTypeTemplates();
+
+            DDMTemplate renderingTemplate = null;
+            if(configuredTemplates.containsKey(fileEntryTypeUuid)) {
+                List<DDMTemplate> ddmTemplates = this.ddmTemplateService.getDDMTemplatesByUuidAndCompanyId(configuredTemplates.get(fileEntryTypeUuid), searchContext.getCompanyId());
+                if(ddmTemplates.size() == 1) {
+                    renderingTemplate = ddmTemplates.get(0);
+                }
+            }
+
+            if(renderingTemplate != null) {
+                HttpServletRequest rq = this.portal.getHttpServletRequest(request);
+                HttpServletResponse rp = this.portal.getHttpServletResponse(response);
+                try {
+                    // Get basic file information
+                    ThemeDisplay themeDisplay = (ThemeDisplay) request.getAttribute(WebKeys.THEME_DISPLAY);
+                    FileEntry fileEntry = this.getFileEntry(searchResultDocument);
+                    FileVersion fileVersion = fileEntry.getFileVersion();
+                    DL dl = DLUtil.getDL();
+
+                    // Get URL and title
+                    String url = dl.getDownloadURL(fileEntry, fileVersion, themeDisplay, StringPool.BLANK, true, false);
+                    String title = searchResultDocument.get(searchContext.getLocale(), Field.TITLE);
+
+                    // Create the ADT context
+                    HashMap<String, Object> ctx = new HashMap<String, Object>(4);
+                    ctx.put(DLFileEntryTemplateVariable.FILE_ENTRY.getVariableName(), fileEntry);
+                    ctx.put(DLFileEntryTemplateVariable.FILE_VERSION.getVariableName(), fileVersion);
+                    ctx.put(DLFileEntryTemplateVariable.FILE_URL.getVariableName(), url);
+                    ctx.put(DLFileEntryTemplateVariable.FILE_PREVIEW_URL.getVariableName(), dl.getPreviewURL(fileEntry, fileVersion, themeDisplay, StringPool.BLANK, true, false));
+                    ctx.put(DLFileEntryTemplateVariable.FILE_IMAGE_PREVIEW_URL.getVariableName(), dl.getImagePreviewURL(fileEntry, fileVersion, themeDisplay));
+                    String rendering = this.portletDisplayTemplate.renderDDMTemplate(rq, rp, renderingTemplate, Collections.emptyList(), ctx);
+                    result = new SearchResult(rendering, url, title);
+                } catch (Exception e) {
+                    throw new SearchResultProcessorException(e, searchResultDocument, "Error during document rendering");
+                }
+            } else {
+                throw new SearchResultProcessorException(searchResultDocument, "Cannot obtain document rendering template. No template found or multiple templates with the same UUID found.");
+            }
+        } catch(PortalException e) {
+            throw new SearchResultProcessorException(e, searchResultDocument, "Cannot obtain document's file entry type");
+        }
+
+        return result;
     }
 
     @Override
     public String getAssetType() {
         return ASSET_TYPE;
+    }
+
+    /**
+     * Returns the file entry related to the indexed document
+     *
+     * @param searchResultDocument The indexed document
+     * @return The file entry related to the indexed document
+     *
+     * @throws SearchResultProcessorException If the file entry is not found
+     */
+    private FileEntry getFileEntry(Document searchResultDocument) throws SearchResultProcessorException {
+        long groupId = Long.parseLong(searchResultDocument.get(Field.GROUP_ID));
+        long fileEntryId = Long.parseLong(searchResultDocument.get(Field.ENTRY_CLASS_PK));
+        FileEntry fileEntry;
+
+        try {
+            fileEntry = this.dlAppService.getFileEntry(fileEntryId);
+        } catch(PortalException e) {
+            throw new SearchResultProcessorException(e, searchResultDocument, "Cannot find DLFileEntry");
+        }
+
+        if(fileEntry.getGroupId() != groupId) {
+            throw new SearchResultProcessorException(searchResultDocument, "Returned DLFileEntry has a groupId different from the one referenced in the index");
+        }
+
+        return fileEntry;
     }
 
 }
--- modules/flashlight-search-service/src/main/java/com/savoirfairelinux/flashlight/service/impl/search/result/template/DLFileEntryTypeTemplateHandler.java
+++ modules/flashlight-search-service/src/main/java/com/savoirfairelinux/flashlight/service/impl/search/result/template/DLFileEntryTypeTemplateHandler.java
@@ -3,17 +3,22 @@ package com.savoirfairelinux.flashlight.service.impl.search.result.template;
 import java.util.LinkedHashMap;
 import java.util.Locale;
 import java.util.Map;
+import java.util.ResourceBundle;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
-import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.language.Language;
 import com.liferay.portal.kernel.portletdisplaytemplate.BasePortletDisplayTemplateHandler;
 import com.liferay.portal.kernel.template.TemplateHandler;
 import com.liferay.portal.kernel.template.TemplateVariableGroup;
 import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
 import com.savoirfairelinux.flashlight.service.FlashlightSearchPortletKeys;
 
+/**
+ * Template handler used to render DLFileEntry search results in Flashlight
+ */
 @Component(
     immediate = true,
     property = {
@@ -22,22 +27,28 @@ import com.savoirfairelinux.flashlight.service.FlashlightSearchPortletKeys;
     service = TemplateHandler.class)
 public class DLFileEntryTypeTemplateHandler extends BasePortletDisplayTemplateHandler {
 
+    private static final String CLASS_NAME = DLFileEntryTypeTemplateHandler.class.getName();
     private static final String VAR_GROUP_NAME = "viewContextVariables";
-    private static final String VAR_GROUP_LABEL = "Context";
+    private static final String VAR_GROUP_LABEL = "File";
 
-    private static final String LANGUAGE_KEY_TEMPLATE = "DLFileEntry search result template";
+    private static final String RES_BUNDLE = "content.Language";
+    private static final String LANGUAGE_KEY_TEMPLATE = "template.name";
 
     @Reference
     private Portal portal;
 
+    @Reference
+    private Language language;
+
     @Override
     public String getClassName() {
-        return this.getClass().getName();
+        return CLASS_NAME;
     }
 
     @Override
     public String getName(Locale locale) {
-        return LanguageUtil.get(locale, LANGUAGE_KEY_TEMPLATE);
+        ResourceBundle bundle = ResourceBundleUtil.getBundle(RES_BUNDLE, locale, this.getClass());
+        return this.language.get(bundle, LANGUAGE_KEY_TEMPLATE);
     }
 
     @Override
@@ -49,6 +60,9 @@ public class DLFileEntryTypeTemplateHandler extends BasePortletDisplayTemplateHa
     public Map<String, TemplateVariableGroup> getTemplateVariableGroups(long classPK, String language, Locale locale) throws Exception {
         Map<String, TemplateVariableGroup> templateVariableGroups = new LinkedHashMap<>();
         TemplateVariableGroup mainGroup = new TemplateVariableGroup(VAR_GROUP_LABEL);
+        for(DLFileEntryTemplateVariable variable : DLFileEntryTemplateVariable.values()) {
+            mainGroup.addVariable(variable.getLabel(), variable.getType(), variable.getVariableName());
+        }
         templateVariableGroups.put(VAR_GROUP_NAME, mainGroup);
         return templateVariableGroups;
     }
